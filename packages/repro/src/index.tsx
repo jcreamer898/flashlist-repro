c4b518f2bf75d62dabdaabe7a332d1e2ec602e7f
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2022-12-09 11:18:14 -0600

    trying this repro

---
+++ packages/repro/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
+
+export const MyList = () => {
+  return (
+    <FlashList
+      data={DATA}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={200}
+    />
+  );
+};
